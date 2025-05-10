d9f9de74f75a132b2c9db6efea77b3cad8755697
João Marcello Aguilar Souza <joao.souza@domhelder.edu.br>
2025-05-09 21:03:04 -0300

    front end atualizado

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,17 +1,19 @@
 import styles from './Main.module.css';
+import 'animate.css'; // Importar Animate.css localmente
 
-function Main(){
+function Main() {
 
-    return(
+    return (
         <main className={styles.mainPage}>
-              <div className={styles.logoMain}>
-                <img src="./src/assets/img/logoMain.png" alt="logomarca"/> 
+            <div className={`${styles.logoMain} animate__animated animate__fadeIn`}>
+                <img src="./src/assets/img/logoMain.png" alt="logomarca" />
             </div>
             <div className={styles.text}>
-            <p>A Justiça do futuro</p>
-            <h1>Resolução de leeds de forma rápida</h1>
+                <p className="animate__animated animate__fadeInDown">A Justiça do futuro</p>
+                <h1 className="animate__animated animate__fadeInDown">Resolução de leeds de forma rápida</h1>
+                <button className="animate__animated animate__fadeInDown">QUERO ENTRAR EM CONTATO</button>
             </div>
-              
+
         </main>
     )
 }
