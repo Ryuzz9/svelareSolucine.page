726f4826265e374d7b3b7b6a8060a992da36e174
Ryuzz9 <marcelloaguilo@gmail.com>
2025-05-11 12:45:50 -0300

    front end add carrousel

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,8 +1,7 @@
 import styles from './Main.module.css';
-import 'animate.css'; // Importar Animate.css localmente
+import 'animate.css';
 
 function Main() {
-
     return (
         <main className={styles.mainPage}>
             <div className={`${styles.logoMain} animate__animated animate__fadeIn`}>
@@ -13,9 +12,32 @@ function Main() {
                 <h1 className="animate__animated animate__fadeInDown">Resolução de leeds de forma rápida</h1>
                 <button className="animate__animated animate__fadeInDown">QUERO ENTRAR EM CONTATO</button>
             </div>
-
+            <div className={styles.carrousel}>
+                <div className={styles.carrouselWrapper}>
+                    <div className={styles.carrouselItem}>
+                        <h1>+60</h1>
+                        <h2>Numero de clientes</h2>
+                    </div>
+                    <div className={styles.carrouselItem}>
+                        <h1>+800</h1>
+                        <h2>Processos na plataforma</h2>
+                    </div>
+                    <div className={styles.carrouselItem}>
+                        <h1>+500</h1>
+                        <h2>Processos finalizados</h2>
+                    </div>
+                    <div className={styles.carrouselItem}>
+                        <h1>+26</h1>
+                        <h2>Seções de mediação</h2>
+                    </div>
+                    <div className={styles.carrouselItem}>
+                        <h1>+800</h1>
+                        <h2>Clientes</h2>
+                    </div>
+                </div>
+            </div>
         </main>
-    )
+    );
 }
 
-export default Main
+export default Main;
