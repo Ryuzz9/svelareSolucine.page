198d404b2dab75ab1691d854b9ab0bcb4570c32d
João Marcello Aguilar Souza <joao.souza@domhelder.edu.br>
2025-05-19 10:55:03 -0300

    atualizarContatos

--- src/Components/Sections/Contato/index.jsx
+++ src/Components/Sections/Contato/index.jsx
@@ -6,24 +6,40 @@ function Contato() {
 
     return (
         <section className={styles.contato}>
-            <div className={styles.logo}>
-                <img src="./src/assets/img/logo.png" alt="logoContato" />
+            <div className={styles.container}>
+                <div className={styles.logo}>
+                    <img src="./src/assets/img/logo.png" alt="logoContato" />
+                    <div className={styles.formulario}>
+                        <h2>Preencha com seus dados <br />abaixo</h2>
+                    </div>
+                    <div className={styles.formularioInfo}>
+                        <label htmlFor="text" className={styles.inputText}>nome</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            id="name"
+                            name="name"
+                        />
+
+                    </div>
+                </div>
+                <div className={styles.tituloContato}>
+                    <img src="./src/assets/img/iconContato.png" alt="iconContato" />
+                    <h1>FALE CONOSCO</h1>
+                </div>
+                <div className={styles.subtitle}>
+                    <h2>CONTATO</h2>
+                    <div className={styles.line}></div>
+                    <p>Selecione abaixo o motivo
+                        de seu contato:</p>
+                    <button className={styles.btn}>Quero ingressar em um processo</button>
+                    <button className={styles.btn}>Quero ingressar em um processo</button>
+                    <button className={styles.btn}>Quero ingressar em um processo</button>
+                    <button className={styles.btn}>Quero ingressar em um processo</button>
+                </div>
             </div>
-            <div className={styles.tituloContato}>
-                <img src="./src/assets/img/iconContato.png" alt="iconContato" />
-                <h1>FALE CONOSCO</h1>
-            </div>
-            <div className={styles.subtitle}>
-                <h2>CONTATO</h2>
-                <div className={styles.line}></div>
-                <p>Selecione abaixo o motivo
-                    de seu contato:</p>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
-            </div>
+
+
 
 
         </section>
