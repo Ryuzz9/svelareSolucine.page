10185e9b524a71721d84ac2f26322363b77cd5a8
João Marcello Aguilar Souza <joao.souza@domhelder.edu.br>
2025-05-21 21:37:21 -0300

    atualizarMobile

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,16 +1,18 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
 import React, { useState, useEffect } from 'react';
 
 function Header() {
-
     const [isSticky, setIsSticky] = useState(false);
-    const [IsAberta, setIsAberta] = useState(false); //crinado um estado para o menu lateral
+    const [IsAberta, setIsAberta] = useState(false);
+    const [isFechar, setIsFechar] = useState(false); // Definição do estado para fechamento do menu
 
-    //função para abrir o menu lateral
     const navbarAberta = () => {
-          setIsAberta(!IsAberta);
+        setIsAberta(!IsAberta);
+    };
 
-    }
+    const navbarFechar = () => {
+        setIsFechar(!isFechar);
+    };
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
@@ -20,24 +22,16 @@ function Header() {
     }, []);
 
     const handleScroll = () => {
-        if (window.pageYOffset > 0) {
-            setIsSticky(true);
-        } else {
-            setIsSticky(false);
-        }
+        setIsSticky(window.pageYOffset > 0);
     };
 
-
     return (
-
         <header className={`${styles.header} ${isSticky ? styles.sticky : ''}`}>
-
-            <div className={`${styles.container} ${IsAberta ? styles.menuAberto : ''}`}>
-                    <div className={styles.fechar}>
-                        <img src="" alt="" />
-                    </div>
+            <div className={`${styles.container} ${IsAberta ? styles.menuAberto : styles.navFechar}`}>
+                <div className={styles.fechar}>
+                    <img src="./src/assets/img/close.svg" alt="Fechar" onClick={navbarAberta} />
+                </div>
                 <nav className={styles.navbarLateral}>
-
                     <ul className={styles.navbar2}>
                         <li><a href="#atuacao">ATUAÇÃO</a></li>
                         <li><a href="#saiba">SAIBA MAIS</a></li>
@@ -45,20 +39,15 @@ function Header() {
                         <li><a href="#parceiros">PARCEIROS</a></li>
                         <li><a href="#contato">FALE CONOSCO</a></li>
                     </ul>
-
                 </nav>
-
-
             </div>
             <div className={styles.logo}>
-                <img src="./src/assets/img/logo.png" alt="logomarca" />
+                <img src="./src/assets/img/logo.png" alt="Logomarca" />
             </div>
             <div className={styles.menu}>
-                <img src="./src/assets/img/menu.svg" alt="" onClick={navbarAberta}/>
+                <img src="./src/assets/img/menu.svg" alt="Menu" onClick={navbarAberta} />
             </div>
-
             <nav className={isSticky ? 'sticky' : ''}>
-
                 <ul className={styles.navbar}>
                     <li><a href="#atuacao">ATUAÇÃO</a></li>
                     <li><a href="#saiba">SAIBA MAIS</a></li>
@@ -67,24 +56,8 @@ function Header() {
                     <li><a href="#contato">FALE CONOSCO</a></li>
                 </ul>
             </nav>
-
-
-
-            <nav className={styles.navbarLateral}>
-
-                <ul className={styles.navbar2}>
-                    <li><a href="#atuacao">ATUAÇÃO</a></li>
-                    <li><a href="#saiba">SAIBA MAIS</a></li>
-                    <li><a href="#sobre">SOBRE</a></li>
-                    <li><a href="#parceiros">PARCEIROS</a></li>
-                    <li><a href="#contato">FALE CONOSCO</a></li>
-                </ul>
-
-            </nav>
-
         </header>
-
-    )
+    );
 }
 
-export default Header
+export default Header;
